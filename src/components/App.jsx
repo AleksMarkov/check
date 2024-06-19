db4a8f3b6fd0717de8b56a96c85a91bc5d1707aa
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-06-19 13:03:23 -0400

    updated Initial template.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,21 @@
+import React from 'react';
+
+function outerFunction(...outerVariable) {
+  const innerVariable = 'inner';
+  console.log('outerarguments: ', arguments);
+  function innerFunction(...innerParameter) {
+    console.log('outerVariable:', outerVariable); // Доступ к переменной из внешнего лексического окружения
+    console.log('innerVariable:', innerVariable); // Доступ к переменной из текущего лексического окружения
+    console.log('innerParameter:', innerParameter); // Доступ к параметру функции
+    console.log('innerarguments: ', arguments);
+  }
+
+  return innerFunction;
+}
+
+const func = outerFunction('outer', 'parameter', 'outer');
+func('parameter', 'outer'); // Выводит: outerVariable: outer, innerVariable: inner, innerParameter: parameter
+
 export const App = () => {
   return (
     <div
@@ -7,7 +25,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
