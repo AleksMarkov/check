452db7462090cc5d9fba5f1cffe2c95061d1558a
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-06-19 13:44:24 -0400

    added function IIFE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,28 @@
 import React from 'react';
+import $ from 'jquery';
 
-function outerFunction(...outerVariable) {
-  const innerVariable = 'inner';
-  console.log('outerarguments: ', arguments);
-  function innerFunction(...innerParameter) {
-    console.log('outerVariable:', outerVariable); // Доступ к переменной из внешнего лексического окружения
-    console.log('innerVariable:', innerVariable); // Доступ к переменной из текущего лексического окружения
-    console.log('innerParameter:', innerParameter); // Доступ к параметру функции
-    console.log('innerarguments: ', arguments);
+var module = (function () {
+  var privateVariable = 'I am private';
+  function privateFunction() {
+    console.log(privateVariable);
   }
+  return {
+    publicMethod: function () {
+      privateFunction();
+    },
+  };
+})();
 
-  return innerFunction;
-}
+module.publicMethod(); // "I am private"
+console.log(module.privateVariable); // undefined
 
-const func = outerFunction('outer', 'parameter', 'outer');
-func('parameter', 'outer'); // Выводит: outerVariable: outer, innerVariable: inner, innerParameter: parameter
+(function ($) {
+  // Код, использующий jQuery, переданный как параметр $
+  $(document).ready(function () {
+    console.log('DOM is ready');
+  });
+})($);
 
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>React homework template</div>;
 };
